fb050a675287c3527a3ed38cf09a83ec10621a00
KierDanielTavas <kian.tavas15g@gmail.com>
2023-06-12 20:11:34 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Century Gothic', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1b424c',
+      },
+    },
+  },
+  plugins: [],
+};
